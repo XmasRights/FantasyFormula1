3415fccbdbc59bfaaf9bbb56ea7e6f5c6cda44e4
XmasRights <chris.fonseka@googlemail.com>
2016-06-11 16:09:13 +0100

    Initial Commit

---
+++ FantasyRacers/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FantasyRacers
+//
+//  Created by Chris Home on 11/06/2016.
+//  Copyright © 2016 Christoher Fonseka. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
