6615f07ee8f66ade20c35d324de3b8403c02b66a
Christopher Fonseka <chris.fonseka@googlemail.com>
2017-09-02 12:16:09 +0100

    Trashed everything, put an empty GUI Application in

---
+++ FantasyRacers/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FantasyRacers
+//
+//  Created by Christopher Fonseka on 02/09/2017.
+//  Copyright © 2017 Christopher Fonseka. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FantasyRacers/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FantasyRacers
+//
+//  Created by Christopher Fonseka on 02/09/2017.
+//  Copyright © 2017 Christopher Fonseka. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
